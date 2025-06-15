e58920bf187ae0d419d8eb5cd2b78eae40c9793e
Wilinton <wilintongabrielss1@gmail.com>
2025-06-15 16:22:53 -0300

    refactor: adiciona credenciais do banco e renomeia classe application

--- src/main/java/com/linkedin_analyzer/Application.java
+++ src/main/java/com/linkedin_analyzer/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LinkedinAnalyzerApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LinkedinAnalyzerApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
