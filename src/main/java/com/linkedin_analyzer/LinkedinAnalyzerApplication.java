44e32bf39fee8e8fbea62e0347a05349b6bfaa54
Wilinton <wilintongabrielss1@gmail.com>
2025-06-15 15:41:25 -0300

    init: commit inicial

---
+++ src/main/java/com/linkedin_analyzer/LinkedinAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.linkedin_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedinAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedinAnalyzerApplication.class, args);
+	}
+
+}
